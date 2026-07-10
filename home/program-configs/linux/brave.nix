9a097d2c5d7870779e43a40068581759aa0ff100
Paul Reitmayer <paul.reitmayer@pm.me>
2026-07-10 19:49:54 +0200

    swap out 7tv for old extension

--- home/program-configs/linux/brave.nix
+++ home/program-configs/linux/brave.nix
@@ -5,7 +5,7 @@
     extensions = [
       { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
       { id = "clngdbkpkpeebahjckkjfobafhncgmne"; } # Stylus
-      { id = "lppmekppnliemjclknbagdhoocikieoi"; } # 7TV
+      { id = "ammjkodgmmoknidbanneddgankgfejfh"; } # 7TV
       { id = "gebbhagfogifgggkldgodflihgfeippi"; } # Return Youtube Dislike
       { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock for Youtube
       { id = "bkkmolkhemgaeaeggcmfbghljjjoofoh"; } # Catppuccin Mocha Theme
